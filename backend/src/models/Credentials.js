b2e24bd4fa746faed6f15a46a65543e67255c010
LUC4R4T0R <luc4r4t0r@gmail.com>
2025-07-14 16:54:07 +0200

    updated backend to express.js v5 and upgraded other dependencies

--- backend/src/models/Credentials.js
+++ backend/src/models/Credentials.js
@@ -1,5 +1,5 @@
 /**
- * this model specifies the format to exchange credentials with the frontend_old
+ * this model specifies the format to exchange credentials with the frontend
  * @param {string} username
  * @param {string} password
  */
